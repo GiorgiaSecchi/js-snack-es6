a198e27e8350594304754c12f2a9b420195255f2
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-25 14:59:03 +0200

    first commit

---
+++ Snack 1/snack-1.js
@@ -0,0 +1,9 @@
+console.log("JS OK");
+
+//# Snack 1
+//* Creare un array di oggetti:
+//
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+// ***********************************************************************************************/
