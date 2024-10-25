1ed5350784c7fa310d25a917d1102a9122aeac2a
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-25 15:38:34 +0200

    1 - array oggetti

--- Snack 1/snack-1.js
+++ Snack 1/snack-1.js
@@ -1,5 +1,3 @@
-console.log("JS OK");
-
 //# Snack 1
 //* Creare un array di oggetti:
 //
@@ -7,3 +5,28 @@ console.log("JS OK");
 // Stampare a schermo la bici con peso minore.
 
 // ***********************************************************************************************/
+
+const racingBike = [
+  (bike1 = {
+    name: "Ciclocross",
+    weight: 7,
+  }),
+  (bike2 = {
+    name: "Ebike",
+    weight: 12,
+  }),
+  (bike3 = {
+    name: "Gravel",
+    weight: 10,
+  }),
+  (bike3 = {
+    name: "Endurance",
+    weight: 8,
+  }),
+  (bike4 = {
+    name: "Aeroad",
+    weight: 9,
+  }),
+];
+
+console.log(racingBike);
